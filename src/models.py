eaf2b11c12a46abb235e41786ed13bc68cb78cfb
PaperMoose <rbrandt810@gmail.com>
2025-02-20 15:54:37 -0800

    updates the pydantic schema

--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List, Optional, Dict, Any
+from datetime import datetime
 
 class ARCTaskOutput(BaseModel):
     output: List[List[int]]
@@ -11,3 +12,45 @@ class ARCPair(BaseModel):
 class ARCTask(BaseModel):
     train: List[ARCPair]
     test: List[ARCPair]
+
+class Message(BaseModel):
+    role: str
+    content: str
+
+class Choice(BaseModel):
+    index: int
+    message: Message
+
+class CompletionTokensDetails(BaseModel):
+    reasoning_tokens: int
+    accepted_prediction_tokens: int
+    rejected_prediction_tokens: int
+
+class Usage(BaseModel):
+    prompt_tokens: int
+    completion_tokens: int
+    total_tokens: int
+    completion_tokens_details: CompletionTokensDetails
+
+class Cost(BaseModel):
+    prompt_cost: float
+    completion_cost: float
+    total_cost: float
+
+class AttemptMetadata(BaseModel):
+    model: str
+    provider: str
+    start_timestamp: datetime
+    end_timestamp: datetime
+    choices: List[Choice]
+    reasoning_tokens: Optional[str] = None
+    kwargs: Dict[str, Any]
+    usage: Usage
+    cost: Cost
+
+class Attempt(BaseModel):
+    metadata: AttemptMetadata
+    answer: str
+
+class Attempts(BaseModel):
+    attempts: List[Attempt]
