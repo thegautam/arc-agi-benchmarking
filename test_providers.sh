2f6842a4cf9c8656d497ef3ac84dd5415dcec29b
PaperMoose <rbrandt810@gmail.com>
2025-03-18 15:25:47 +1100

    updates

--- test_providers.sh
+++ test_providers.sh
@@ -34,13 +34,8 @@ echo "Starting provider tests..."
 
 # Define test configurations
 CONFIGS=(
-    "gpt4o_mini e7639916"
-    "gpt4o_standard 2037f2c7"
     "gpt-4.5-2025-02-21-alpha 14754a24"
     "o1_pro-2025-02-25 7bb29440"
-    # "claude_sonnet e7639916" 
-    # "gemini_flash e7639916"
-    # "deepseek_chat e7639916"
 )
 
 # Create temporary file with active configurations
